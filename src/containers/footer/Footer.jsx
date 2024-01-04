cedb0a213db27eab5c72870404a4447c115a2fd8
unknown <jamesagu2001@gmail.com>
2024-01-04 08:08:18 -0800

    third commit

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
       <div className="gpt3__footer-links">
         <div className="gpt3__footer-links_logo">
           <img src={gpt3Logo} alt="logo" />
-          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+          <p>21 Road G Close Festac Town, All Rights Reserved</p>
         </div>
         <div className="gpt3__footer-links_div">
           <h4>Links</h4>
@@ -33,13 +33,13 @@ const Footer = () => {
         </div>
         <div className="gpt3__footer-links_div">
           <h4>Get in touch</h4>
-          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>21 Road G Close Festac Town</p>
           <p>085-132567</p>
-          <p>info@payme.net</p>
+          <p>agujames8@gmail.com</p>
         </div>
       </div>
       <div className="gpt3__footer-copyright">
-        <p>© 2021 GPT-3. All rights reserved.</p>
+        <p>© 2024 GPT-3. All rights reserved.</p>
       </div>
     </div>
   )
